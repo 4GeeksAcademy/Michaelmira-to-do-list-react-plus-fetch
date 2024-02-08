8787123f13de909718cd9c2b7cc9ca46ce595896
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-08 21:17:34 +0000

    add useState input value and handleInputChange and preventDefault

--- src/js/component/ToDo.jsx
+++ src/js/component/ToDo.jsx
@@ -2,26 +2,44 @@
 
 import PropTypes from 'prop-types';
 
-import React from "react";
+import React, { useState } from 'react';
 
 function ToDo(props) {
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        console.log("Submitted Value", inputValue);
+    };
+
     const inputStyle = {
         border: 'none',  // Remove the border
         outline: 'none',  // Remove the default focus outline
-    }
+    };
     
     return (
         <div >
-            <form action="/submit" method="post">
+            <form onSubmit= {handleFormSubmit}>
                 <label for="name"></label>
-                <input className='text-body-tertiary' type="text" placeholder="What needs to be done? " required style={inputStyle}  ></input>
+                <input 
+                className='text-body-tertiary' 
+                type="text" 
+                placeholder="What needs to be done? " 
+                required 
+                style={inputStyle}
+                value={inputValue}
+                onChange={handleInputChange}></input>
             </form>
         </div>
     );
 };
 
 ToDo.PropTypes = {
-    hundredThousandsDigit: PropTypes.string,
+    inputStick: PropTypes.string,
     tenThousandsDigit: PropTypes.number,
     thousandsDigit: PropTypes.number,
     hundredsDigit: PropTypes.number,
