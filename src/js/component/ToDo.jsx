630686e11c0285c60416cf05abb0d05f8ac46d87
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-10 02:06:32 +0000

    styling is all messed up after import of my logic. some style done needs more

--- src/js/component/ToDo.jsx
+++ src/js/component/ToDo.jsx
@@ -6,7 +6,7 @@ import React, { useState, useEffect } from 'react';
 function TodoItem({label, is_done, delete_todo, toggle_todo}){
 	return (
         <>
-            <div className="todo-item">
+            <div className="todo-item bg-white border d-flex align-items-center justify-content-between w-100">
                 <input type="checkbox"checked={is_done} onChange={toggle_todo} />
                 <span className="todo-text"> {label} </span>
                 <button type="button" className="btn btn-danger" onClick= {delete_todo}>
@@ -36,82 +36,67 @@ function ToDoPlusCancel() {
     const inputStyle = {
         border: 'none',  // Remove the border
         outline: 'none',  // Remove the default focus outline
+        width: "100",
     };
-
-    const buttonStyle = {
-        border: "none",
-        outline: "none,"
-    };
-
-    
     
     return (
         <>
-            <div className="div row align-items-center fs-4 ">
-                <div className="col-10">
-                    <form 
-                    onSubmit={(ev) => {
-                        ev.preventDefault();
-                        if (todoInput.length > 0) {
-                            setTodos([
-                                {
-                                    label: todoInput,
-                                    is_done: false,
-                                },
-                                ...todos,
-                            ]);
-                            setTodoInput("");
-                        }
-                    }} 
-                    >
-                        <label htmlFor="toDoInput"></label>
-                        <input 
-                            id="toDoInput"
-                            className='text-body-tertiary' 
-                            type="text" 
-                            placeholder="What needs to be done? " 
-                            required 
-                            style={inputStyle}
-                            value={todoInput}
-                            onChange={ev => setTodoInput(ev.target.value)}
+            <div className="div row align-items-center fs-4 w-100 ">
+                <div className="col-12">
+                    <div className="row">
+                        <form 
+                        onSubmit={(ev) => {
+                            ev.preventDefault();
+                            if (todoInput.length > 0) {
+                                setTodos([
+                                    {
+                                        label: todoInput,
+                                        is_done: false,
+                                    },
+                                    ...todos,
+                                ]);
+                                setTodoInput("");
+                            }
+                        }}
+                        className="w-100" 
                         >
-                        </input>
-                        {todos.map((item, idx) => (
-					        <TodoItem 
-                                key={idx}
-                                label={item.label}
-                                is_done={item.is_done}
-                                toggle_todo={() => 
-							        setTodos(
-								        todos.toSpliced(idx, 1, {
-                                            label: item.label,
-                                            is_done: !item.is_done,
-								        })
-							        )
-						        }
-						        delete_todo={() => 
-							        setTodos(
-								        todos.toSpliced(idx, 1
-								        )
-							        )
-						        }
-					        />
-				))}
-				<small>{todos.filter((item) => item.is_done).length} todos left to do</small>
-                    </form>
+                            <label htmlFor="toDoInput"></label>
+                            <input 
+                                id="toDoInput"
+                                className='text-body-tertiary' 
+                                type="text" 
+                                placeholder="What needs to be done? " 
+                                required 
+                                style={inputStyle}
+                                value={todoInput}
+                                onChange={ev => setTodoInput(ev.target.value)}
+                            >
+                            </input>
+                            {todos.map((item, idx) => (
+                                <TodoItem 
+                                    key={idx}
+                                    label={item.label}
+                                    is_done={item.is_done}
+                                    toggle_todo={() => 
+                                        setTodos(
+                                            todos.toSpliced(idx, 1, {
+                                                label: item.label,
+                                                is_done: !item.is_done,
+                                            })
+                                        )
+                                    }
+                                    delete_todo={() => 
+                                        setTodos(
+                                            todos.toSpliced(idx, 1
+                                            )
+                                        )
+                                    }
+                                />
+                            ))}
+                            <small>{todos.filter((item) => item.is_done).length} todos left to do</small>
+                        </form>
+                    </div>                    
                 </div>     
-                <div className="col-2 text-right">
-                    <div style={{ textAlign: 'right' }}>
-                        <button 
-                            type="button " 
-                            className="btn btn-sm btn-light text-body-tertiary"
-                            id="eraseButton"
-                            style = {buttonStyle}
-                        >
-                            X 
-                        </button>
-                    </div>
-                </div>
             </div>
         </>
     );
