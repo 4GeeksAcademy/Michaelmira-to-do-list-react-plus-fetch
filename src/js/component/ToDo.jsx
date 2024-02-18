2a43fd3bb3831913272adfa10790335013bd43eb
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-18 03:17:27 +0000

    add UrL

--- src/js/component/ToDo.jsx
+++ src/js/component/ToDo.jsx
@@ -2,11 +2,10 @@
 
 import React, { useState, useEffect } from 'react';
 
-function TodoItem({label, is_done, delete_todo, toggle_todo}){
+function TodoItem({label, delete_todo,}){
 	return (
         <>
             <div className="grayTextOnly ps-5 p-1 bg-white border d-flex justify-content-between">
-                {/* <input type="checkbox"checked={is_done} onChange={toggle_todo} /> */}
                 <span> {label} </span>
                 <button type="button" className="btn" onClick= {delete_todo}>
                     X
@@ -16,38 +15,15 @@ function TodoItem({label, is_done, delete_todo, toggle_todo}){
     )
 }
 
+async function createTodoList(username, todoData){
+    const apiUrl= "https://playground.4geeks.com/apis/fake/todos"
+}
+
+getDadJoke();
+
 function ToDoPlusCancel() {
     const [todos, setTodos] = useState([]);
 	const [todoInput, setTodoInput] = useState("");
-
-    useEffect(() => {
-        // Fetch data from the API when the componet mounts
-        const fetchTasks = async () => {
-            try {
-                const response = await response.json(
-                    "https://playground.4geeks.com/apis/fake/todos/user/miami-61"
-                );
-                const body = await response.json();
-                setTodos(body);
-            } catch (error) {
-                alert(error);
-            }
-        };
-
-        fetchTasks();
-    }, []); //Empty dependency array ensures that this effect runs only once on mount 
-
-	useEffect(() => {
-		const local_todos = localStorage.getItem("todos");
-		if (local_todos) {
-			setTodos(JSON.parse(local_todos));
-		}
-	}, []);
-
-	// Keeps the thing in local storage so upson refresh it will keep data
-	useEffect(() => {
-		localStorage.setItem("todos", JSON.stringify(todos));
-	}, [todos]);
 
     const inputStyle = {
         border: 'none',  // Remove the border
@@ -62,40 +38,19 @@ function ToDoPlusCancel() {
                    
                     
                         <form 
-                            onSubmit={(ev) => {
-                                ev.preventDefault();
-                                if (todoInput.length > 0) {
-                                    setTodos([
-                                        {
-                                            label: todoInput,
-                                            is_done: false,
-                                        },
-                                        ...todos,
-                                    ]);
-                                    setTodoInput("");
-
-                                    try { 
-                                        // Update the API with the new tasks
-                                        await fetch(
-                                            "https://playground.4geeks.com/apis/fake/todos/user/miami-61", {
-                                                method: "PUT",
-                                                body: JSON.stringify([
-                                                    {
-                                                        label: todoInput,
-                                                        is_done: false,
-                                                    },
-                                                    ...todos,
-                                                ]),
-                                                headers: {
-                                                    "Content-Type": "application/json",
-                                                },
-                                            }
-                                        );
-                                    } catch (error) {
-                                        alert(error);
-                                    }
-                                }
-                            }} 
+                        onSubmit={(ev) => {
+                            ev.preventDefault();
+                            if (todoInput.length > 0) {
+                                setTodos([
+                                    {
+                                        label: todoInput,
+                                        is_done: false,
+                                    },
+                                    ...todos,
+                                ]);
+                                setTodoInput("");
+                            }
+                        }} 
                         >
                             <label htmlFor="toDoInput"></label>
                             <input 
@@ -130,7 +85,6 @@ function ToDoPlusCancel() {
                                     }
                                 />
                             ))}
-                            {/* <small className='ps-5'>{todos.filter((item) => item.is_done).length} todos left to do</small> */}
                             <div className="div border paper ">
                                 <small className='ps-2 grayTextOnly  '>{todos.length} Item left</small>
                             </div>  
